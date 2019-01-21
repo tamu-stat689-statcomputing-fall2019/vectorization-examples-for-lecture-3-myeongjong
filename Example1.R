f320112fb029e389c9ef85c6144974c582cba503
Irina Gaynanova <irinag@stat.tamu.edu>
2019-01-21 16:16:27 -0600

    Small typo correction, explicit return

--- Example1.R
+++ Example1.R
@@ -2,17 +2,23 @@
 
 square_for <- function(x){
   # [ToDo] Use the for loop
+  
+  return(y)
 }
 
 square_sapply <- function(x){
   # [ToDo] Use the sapply function
+  
+  return(y)
 }
 
 square_vec <- function(x){
   # [ToDo] Use power function in vector form
+  
+  return(y)
 }
 
-# [ToDo] Create a vector x of size 100,000
+# [ToDo] Create a vector x of size 100,000 of normal variables
 
 # [ToDo] Verify that all functions return the same output
 
