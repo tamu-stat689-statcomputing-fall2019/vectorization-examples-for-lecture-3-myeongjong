36af76cea7cabf630a6fe0a1577355c84d59811e
Irina Gaynanova <irinag@stat.tamu.edu>
2019-01-21 15:46:20 -0600

    Created Example 1 starter code

---
+++ Example1.R
@@ -0,0 +1,19 @@
+# Squaring elements of a given vector
+
+square_for <- function(x){
+  # [ToDo] Use the for loop
+}
+
+square_sapply <- function(x){
+  # [ToDo] Use the sapply function
+}
+
+square_vec <- function(x){
+  # [ToDo] Use power function in vector form
+}
+
+# [ToDo] Create a vector x of size 100,000
+
+# [ToDo] Verify that all functions return the same output
+
+# [ToDo] Use microbenchmark package to compare three functions in terms of speed
