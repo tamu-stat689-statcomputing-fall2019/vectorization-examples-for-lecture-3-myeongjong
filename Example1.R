15594b0af716c7040c096323edb24b3178880ef2
Irina Gaynanova <irinag@stat.tamu.edu>
2019-01-24 14:14:38 -0600

    Filled code for Example 1

--- Example1.R
+++ Example1.R
@@ -34,6 +34,26 @@ square_vec2 <- function(x){
 
 # [ToDo] Create a vector x of size 100,000 of normal variables
 
+x <- rnorm(100000)
+
 # [ToDo] Verify that all functions return the same output
+y1 <- square_for(x)
+y2 <- square_sapply(x)
+y3 <- square_vec(x)
+y4 <- square_vec2(x)
+
+identical(y1, y2)
+identical(y1, y3)
+identical(y1, y4)
+
+sum(abs(y1-y2))
 
 # [ToDo] Use microbenchmark package to compare three functions in terms of speed
+library(microbenchmark)
+
+microbenchmark(
+  square_for(x),
+  square_sapply(x),
+  square_vec(x),
+  square_vec2(x)
+)
